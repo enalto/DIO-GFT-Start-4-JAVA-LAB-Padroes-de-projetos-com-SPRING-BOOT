11ac25b9da429082ce0ca1bb7a760f9d569a2544
enalto <enaltogondrige@gmail.com>
2022-03-05 22:26:22 -0300

    implementação clienteService, Repositórios.

--- src/main/java/one/digitalinnovation/gof/Application.java
+++ src/main/java/one/digitalinnovation/gof/Application.java
@@ -2,12 +2,16 @@ package one.digitalinnovation.gof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import jdk.jfr.Enabled;
+
+@EnableFeignClients
 @SpringBootApplication
-public class LabPadroesProjetoSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
