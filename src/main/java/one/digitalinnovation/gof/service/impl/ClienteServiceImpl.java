54cf243e3363a7c5e9e501e579a22f1d891c9cec
enalto <enaltogondrige@gmail.com>
2022-03-05 23:21:25 -0300

    clienteServiceImpl implementação dos métodos

--- src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
+++ src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
@@ -1,40 +1,74 @@
 package one.digitalinnovation.gof.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.model.ClienteRepository;
+import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovation.gof.model.EnderecoRepository;
 import one.digitalinnovation.gof.service.ClienteService;
+import one.digitalinnovation.gof.service.ViaCepService;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private ViaCepService viaCepService;
+
 	@Override
 	public Iterable<Cliente> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		return clienteRepository.findAll();
 	}
 
 	@Override
 	public Cliente buscarPorId(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return clienteRepository.findById(id).orElseThrow();
 	}
 
 	@Override
 	public void inserir(Cliente cliente) {
-		// TODO Auto-generated method stub
+		salvarClienteComCep(cliente);
 
+	}
+
+	private void salvarClienteComCep(Cliente cliente) {
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			Endereco novoEndereco = viaCepService.consultaCep(cep);
+			if (!novoEndereco.equals(null)) {
+				enderecoRepository.save(novoEndereco);
+			}
+			return novoEndereco;
+		});
+
+		cliente.setEndereco(endereco);
+
+		clienteRepository.save(cliente);
 	}
 
 	@Override
 	public void atualizar(Long id, Cliente cliente) {
-		// TODO Auto-generated method stub
+
+		clienteRepository.findById(id).ifPresent(cli -> {
+			salvarClienteComCep(cliente);
+		});
 
 	}
 
 	@Override
 	public void deletar(Long id) {
-		// TODO Auto-generated method stub
+
+//		clienteRepository.findById(id).ifPresent(cliente -> {
+//			clienteRepository.delete(cliente);
+//		});
+		clienteRepository.deleteById(id);
 
 	}
 
